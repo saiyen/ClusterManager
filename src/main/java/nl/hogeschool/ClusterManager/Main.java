24c4d0c751e1c1c1736096941d34dde294663d5b
ownacademy <ivanivanov1992@gmail.com>
2016-05-15 19:14:44 +0200

    JSch logger

--- src/main/java/nl/hogeschool/ClusterManager/Main.java
+++ src/main/java/nl/hogeschool/ClusterManager/Main.java
@@ -1,13 +1,16 @@
 package nl.hogeschool.ClusterManager;
 
 import Connection.*;
+import Logger.JschLogger;
 import ReadConfig.ReadConfig;
+import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] Args){
         try {
+            JSch.setLogger(new JschLogger());
             ReadConfig read = new ReadConfig();
             read.getConnectionProperties();
 
