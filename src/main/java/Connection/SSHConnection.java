a78e2bf75e1fb758d79e5d84c55ce2e06fab6714
ownacademy <ivanivanov1992@gmail.com>
2016-05-26 10:28:52 +0200

    Check if host is verifiable

--- src/main/java/Connection/SSHConnection.java
+++ src/main/java/Connection/SSHConnection.java
@@ -3,10 +3,13 @@ package Connection;
 import Interfaces.IConnection;
 import Models.SSHConnectionModel;
 import ReadConfig.ReadConfig;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.DisconnectReason;
+import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.userauth.keyprovider.KeyProvider;
 
 public class SSHConnection implements IConnection{
@@ -23,7 +26,21 @@ public class SSHConnection implements IConnection{
             SSHClient sshConnection;
             sshConnection = new SSHClient();
             sshConnection.loadKnownHosts();
-            sshConnection.connect(currentConnection.getHost());
+            
+            try {
+                sshConnection.connect(currentConnection.getHost());
+            } catch (TransportException e) {
+                if (e.getDisconnectReason() == DisconnectReason.HOST_KEY_NOT_VERIFIABLE) {
+                    String msg = e.getMessage();
+                    String[] split = msg.split("`");
+                    String vc = split[3];
+                    sshConnection = new SSHClient();
+                    sshConnection.addHostKeyVerifier(vc);
+                    sshConnection.connect(currentConnection.getHost());
+                } else {
+                    throw e;
+                }
+            }
 
             KeyProvider loadKey = sshConnection.loadKeys(keyPath, currentConnection.getPassphrase());
             sshConnection.authPublickey(currentConnection.getUser(), loadKey);
